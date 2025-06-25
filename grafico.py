91228903b2ca04a466db99afb5e882ca4182309c
Luis Ignacio Junior <128869475+oluisjr@users.noreply.github.com>
2025-06-25 15:46:16 +0000

    modified:   grafico.py 	modified:   grafico.py

--- grafico.py
+++ grafico.py
@@ -4,6 +4,7 @@ import streamlit as st
 from io import BytesIO
 from PIL import Image
 import pandas as pd
+from datetime import datetime
 import numpy as np
 import os
 from openpyxl import Workbook, load_workbook
